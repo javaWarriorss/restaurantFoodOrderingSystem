b668a55fd43deb85fe8fe0f6ccc4b2ff2e8eb433
kristine kaposte <kika217@inbox.lv>
2023-02-03 19:29:34 +0200

    1th empty commit

---
+++ src/test/java/com/example/restaurantfoodorderingsystem/RestaurantFoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantfoodorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantFoodOrderingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
