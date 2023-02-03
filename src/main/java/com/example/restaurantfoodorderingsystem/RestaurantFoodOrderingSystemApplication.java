1e743a632f2ef154c3a5ed6060b999dc8d28dad6
kristine kaposte <kika217@inbox.lv>
2023-02-03 19:31:06 +0200

    1th empty commit

--- src/main/java/com/example/restaurantfoodorderingsystem/RestaurantFoodOrderingSystemApplication.java
+++ src/main/java/com/example/restaurantfoodorderingsystem/RestaurantFoodOrderingSystemApplication.java
@@ -8,6 +8,8 @@ public class RestaurantFoodOrderingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantFoodOrderingSystemApplication.class, args);
+
+
     }
 
 }
