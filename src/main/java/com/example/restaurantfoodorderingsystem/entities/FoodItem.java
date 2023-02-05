e22640b8db49647896d86f70024530e9747fde0b
Liga Avotina <liiga.avotina@gmail.com>
2023-02-05 15:14:00 +0200

    Created FoodItem.java

---
+++ src/main/java/com/example/restaurantfoodorderingsystem/entities/FoodItem.java
@@ -0,0 +1,22 @@
+package com.example.restaurantfoodorderingsystem.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // for empty constructor
+@AllArgsConstructor // for constructor
+@Data // for getters and setters
+@Entity // connecting fields with database
+
+public class FoodItem {
+    @Id // mark class as entity
+    @GeneratedValue(strategy = GenerationType.AUTO) //  generate value and specify how counting should happen using strategy
+    private Long id;
+    private String foodName;
+    private String description;
+    private Integer price;
+    private String category; // for example breakfast, dinner, special offer etc.
+    private String type; // salads, pancakes, main dishes
+}
