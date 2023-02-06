a8f9553226cbde6a54111f4f7a795c7f530c5535
marialazutina <work.lazutina@gmail.com>
2023-02-06 21:12:33 +0200

    6FEB
    Admin/Orde class/controller/html

--- src/main/java/com/example/restaurantfoodorderingsystem/entities/Admin.java
+++ src/main/java/com/example/restaurantfoodorderingsystem/entities/Admin.java
@@ -1,6 +1,62 @@
 package com.example.restaurantfoodorderingsystem.entities;
 
-//MARIJA
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long adminId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
     private String email;
+    @Column(unique = true)
+    private String password;
+    @Column()
+    private Boolean active;
+
+    public Long getAdminId() {
+        return adminId;
+    }
+    public void setId(Long adminId) {
+        this.adminId = adminId;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
+
--- src/main/java/com/example/restaurantfoodorderingsystem/entities/Order.java
+++ src/main/java/com/example/restaurantfoodorderingsystem/entities/Order.java
@@ -1,4 +1,36 @@
 package com.example.restaurantfoodorderingsystem.entities;
-//MARIJA
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //    @GeneratedValue()
+    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "cutomer_id")
+    private Customer customer;
+    @ManyToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "foodItem_id")
+    private FoodItem foodItem;
+    private Integer quantity;
+    //    private Double price;
+    private String note;
+    @CreationTimestamp
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+    private boolean orderStatus;
+//private String payment;
 }
