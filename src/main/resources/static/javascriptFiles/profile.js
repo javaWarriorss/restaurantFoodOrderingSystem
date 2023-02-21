9f1ff0c2a7a7eacda1538aafa465035ac4d6ccec
Kristinekaposte <kika217@inbox.lv>
2023-02-21 10:42:28 +0200

    all code with styles works

--- src/main/resources/static/javascriptFiles/profile.js
+++ src/main/resources/static/javascriptFiles/profile.js
@@ -1,4 +1,4 @@
-var loadFile = function (event) {
-    var image = document.getElementById("output");
-    image.src = URL.createObjectURL(event.target.files[0]);
-};
+// var loadFile = function (event) {
+//     var image = document.getElementById("output");
+//     image.src = URL.createObjectURL(event.target.files[0]);
+// };
