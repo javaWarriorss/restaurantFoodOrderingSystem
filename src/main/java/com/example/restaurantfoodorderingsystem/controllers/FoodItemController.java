bbe393dfaefe7ae342ccdde1f004356ff395b500
Kristinekaposte <kika217@inbox.lv>
2023-02-22 14:47:15 +0200

    working code 3

--- src/main/java/com/example/restaurantfoodorderingsystem/controllers/FoodItemController.java
+++ src/main/java/com/example/restaurantfoodorderingsystem/controllers/FoodItemController.java
@@ -5,6 +5,7 @@ import com.example.restaurantfoodorderingsystem.services.AdminService;
 import com.example.restaurantfoodorderingsystem.services.FoodItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -244,38 +245,49 @@ public class FoodItemController {
     // _____________________Kristine method___________________________________
     //this method opens page after login and shows all meals
     @GetMapping("menu/{customerId}")
-    public String displayPageAfterLoginShowAllMeals(@PathVariable  Long customerId, Model model){
-        model.addAttribute("customerId",customerId);
-        model.addAttribute("foodItemList", this.foodItemService.getAllFoodItems());
-        return "menu";
+    public String displayPageAfterLoginShowAllMeals(@PathVariable  Long customerId, Model model,
+                                                    @CookieValue(value = "customerCookie")String customerIdFromCookie){
+        try {
+            // System.out.println("cookie: " +customerIdFromCookie);
+            model.addAttribute("customerId",customerIdFromCookie);
+            // model.addAttribute("customerCookie",customerService.findAllCustomersById(Long.valueOf(customerIdFromCookie)));
+            model.addAttribute("foodItemList", this.foodItemService.getAllFoodItems());
+            return "menu";
+        }catch (Exception e){
+            return "redirect:/login?message=customer_not_found";
+
+        }
+
+
     }
     @GetMapping("menu/{customerId}/contactUs")
-    public String displayContactUs(@PathVariable  Long customerId, Model model){
-        model.addAttribute("customerId",customerId);
+    public String displayContactUs(@PathVariable  Long customerId, Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) throws Exception {
+        // model.addAttribute("customerId",customerId);
+        model.addAttribute("customerId",customerIdFromCookie);
         return "customer/contactUs";
     }
 
     //________________Menu_Page_Liga_________________________________________
 
     @GetMapping("menu/{customerId}/menuMeatMainDishes")
-    public String showMenuAllMeatMainDishes(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllMeatMainDishes(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Meat main dishes";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu"; // YOU NEED TO CHANGE ONLT THESE PATHS, PROBABLY STH LIKE  return "yourMapName/menu";
     }
 
     @GetMapping("menu/{customerId}/menuFishMainDishes")
-    public String showMenuAllFishMainDishes(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllFishMainDishes(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Fish main dishes";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuVegetableDishes")
-    public String showMenuAllVegetableDishes(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllVegetableDishes(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Vegetable dishes";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
@@ -283,8 +295,8 @@ public class FoodItemController {
 
 
     @GetMapping("menu/{customerId}/menuSideDishes")
-    public String showMenuAllSideDishes(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllSideDishes(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Side dishes";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
@@ -292,24 +304,24 @@ public class FoodItemController {
 
 
     @GetMapping("menu/{customerId}/menuSoups")
-    public String showMenuAllSoups(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllSoups(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Soups";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuSalads")
-    public String showMenuAllSalads(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllSalads(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Salads";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuPancakes")
-    public String showMenuAllPancakes(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllPancakes(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Pancakes";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
@@ -317,32 +329,32 @@ public class FoodItemController {
 
 
     @GetMapping("menu/{customerId}/menuBurgers")
-    public String showMenuAllBurgers(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllBurgers(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Burgers";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuSnacks")
-    public String showMenuAllSnacks(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllSnacks(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Snacks";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuSauces")
-    public String showMenuAllSauces(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllSauces(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Sauces";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuBread")
-    public String showMenuAllBread(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllBread(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Bread";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
@@ -357,8 +369,8 @@ public class FoodItemController {
 //    }
 
     @GetMapping("menu/{customerId}/menuBakery")
-    public String showMenuAllBakery(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllBakery(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Bakery";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
@@ -366,8 +378,8 @@ public class FoodItemController {
 
 
     @GetMapping("menu/{customerId}/menuCutlery")
-    public String showMenuAllCutlery(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllCutlery(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemType = "Cutlery";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByType(foodItemType));
         return "menu";
@@ -377,40 +389,40 @@ public class FoodItemController {
     // by category
 
     @GetMapping("menu/{customerId}/menuDrinks")
-    public String showMenuAllDrinks(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuAllDrinks(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemCategory = "Drinks";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByCategory(foodItemCategory));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuBreakfast")
-    public String showMenuBreakfast(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuBreakfast(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemCategory = "Breakfast";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByCategory(foodItemCategory));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuBusinessLunch")
-    public String showMenuBusinessLunch(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuBusinessLunch(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemCategory = "Business Lunch";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByCategory(foodItemCategory));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuDesserts")
-    public String showMenuDesserts(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuDesserts(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemCategory = "Desserts";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByCategory(foodItemCategory));
         return "menu";
     }
 
     @GetMapping("menu/{customerId}/menuSpecialOffer")
-    public String showMenuSpecialOffer(@PathVariable  Long customerId,Model model) {
-        model.addAttribute("customerId",customerId);
+    public String showMenuSpecialOffer(@PathVariable  Long customerId,Model model,@CookieValue(value = "customerCookie")String customerIdFromCookie) {
+        model.addAttribute("customerId",customerIdFromCookie);
         String foodItemCategory = "Special Offer";
         model.addAttribute("foodItemList", this.foodItemService.getAllFoodItemsByCategory(foodItemCategory));
         return "menu";
