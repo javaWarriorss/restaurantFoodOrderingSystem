d80b58ddd49e83d9a922e5fb8c2af31d86f62807
Kristinekaposte <kika217@inbox.lv>
2023-02-23 19:46:21 +0200

    admin id changed to identity

--- src/main/java/com/example/restaurantfoodorderingsystem/entities/Admin.java
+++ src/main/java/com/example/restaurantfoodorderingsystem/entities/Admin.java
@@ -9,7 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 public class Admin {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
